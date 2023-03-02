71feef6aa1141f7e0dd0c6ad8ed351826c8b28b7
Alex Johnson <johnsonav1992@gmail.com>
2023-03-02 12:59:11 -0600

    setting up logic

---
+++ src/logic/actions.js
@@ -0,0 +1,7 @@
+export const ACTIONS = {
+    INPUT_DIGIT: "input-digit",
+    CHOOSE_OPERATION: "choose-operation",
+    CLEAR: "clear",
+    DELETE_DIGIT: "delete-digit",
+    EVALUATE: "evaluate",
+}
