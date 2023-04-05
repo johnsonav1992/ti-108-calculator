d278e22ea18b9a460d3d10a14c97e595411a9e4f
Alex Johnson <johnsonav1992@gmail.com>
2023-04-05 06:35:10 -0500

    negative placement

--- src/components/Screen/Screen.jsx
+++ src/components/Screen/Screen.jsx
@@ -4,9 +4,15 @@ import classes from './Screen.module.css'
 
 const Screen = () => {
   const { currentOperand } = useContext(calcContext);
+  const isNegative = parseFloat(currentOperand) < 0;
+
+  const formattedOperand = isNegative ? currentOperand.replace(/^./, "") : ''
 
   return (
-    <div className={classes.screen}>{currentOperand}</div>
+    <div className={classes.screen}>
+      {isNegative && <div className={ classes.negative }> - </div>}
+      {isNegative ? formattedOperand : currentOperand}
+    </div>
   )
 }
 
