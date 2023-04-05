8317615f9572b3b92da7982736f8a63938f8e403
Alex Johnson <johnsonav1992@gmail.com>
2023-04-04 21:11:54 -0500

    formatting

--- src/logic/reducer.js
+++ src/logic/reducer.js
@@ -29,39 +29,39 @@ export function reducer(state, { type, payload }) {
             if (state.operationChosen) {
                 const numberToTest = state.decimalClicked ? state.previousOperand : state.currentOperand;
                 const containsDigitsAfterDecimal = /^\d+\.\d+$/.test(numberToTest);
-                
+
                 if (payload.digit === ".") {
-                return containsDigitsAfterDecimal
-                    ? { ...state }
-                    : {
-                        ...state,
-                        decimalClicked: true,
-                        previousOperand: state.currentOperand,
-                        currentOperand: "0."
-                    };
+                    return containsDigitsAfterDecimal
+                        ? { ...state }
+                        : {
+                            ...state,
+                            decimalClicked: true,
+                            previousOperand: state.currentOperand,
+                            currentOperand: "0."
+                        };
                 }
             
                 const decimal = state.currentOperand.indexOf(".");
             
                 if (decimal > -1 && state.decimalClicked) {
-                return {
-                    ...state,
-                    currentOperand:
-                    state.currentOperand.length - decimal <= 8
-                        ? `${state.currentOperand}${payload.digit}`
-                        : state.currentOperand
-                };
+                    return {
+                        ...state,
+                        currentOperand:
+                        state.currentOperand.length - decimal <= 8
+                            ? `${state.currentOperand}${payload.digit}`
+                            : state.currentOperand
+                    };
                 }
             
                 return {
-                ...state,
-                decimalClicked: false,
-                currentOperand:
-                    state.currentOperand === "0."
-                    ? `${payload.digit}.`
-                    : state.currentOperand !== "0." && state.currentOperand !== state.previousOperand
-                    ? `${state.currentOperand.slice(0, -1) || ""}${payload.digit}.`
-                    : `${payload.digit}.`
+                    ...state,
+                    decimalClicked: false,
+                    currentOperand:
+                        state.currentOperand === "0."
+                        ? `${payload.digit}.`
+                        : state.currentOperand !== "0." && state.currentOperand !== state.previousOperand
+                        ? `${state.currentOperand.slice(0, -1) || ""}${payload.digit}.`
+                        : `${payload.digit}.`
                 };
             }
             
