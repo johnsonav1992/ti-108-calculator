a095534e6c74b8411e12b1b559717da5e936a788
Alex Johnson <johnsonav1992@gmail.com>
2023-04-30 21:10:52 -0500

    division by 0 test

--- cypress/e2e/edgeCases.cy.js
+++ cypress/e2e/edgeCases.cy.js
@@ -11,4 +11,20 @@ describe( 'Edge cases', () => {
             expect( str ).to.equal( `Turn the calculator on first!` );
         } );
     } );
+
+    it( 'errors out of user divides a number by 0', () => {
+        cy.get( '[data-cy="ON/C_button"]' ).click();
+        cy.get( '[data-cy="screen"]' ).should( 'contain', '0.' );
+
+        cy.get( '[data-cy="5_button"]' ).click();
+        cy.get( '[data-cy="screen"]' ).should( 'contain', '5.' );
+
+        cy.get( '[data-cy="÷_button"]' ).click();
+
+        cy.get( '[data-cy="0_button"]' ).click();
+        cy.get( '[data-cy="=_button"]' ).click();
+
+        cy.get( '[data-cy="screen"]' ).should( 'contain', 'E' );
+
+    } );
 } );
